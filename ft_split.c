d8bb933dae22d736d3326cbc6c48007c3271323f
Toshkumar Sharma <tsharma@c2r7p3.42nice.fr>
2022-04-28 20:30:01 +0200

    Fixed till split except for strlcpy.

--- ft_split.c
+++ ft_split.c
@@ -3,59 +3,104 @@
 /*                                                        :::      ::::::::   */
 /*   ft_split.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: tsharma <marvin@42.fr>                     +#+  +:+       +#+        */
+/*   By: tsharma <tsharma@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/27 19:12:27 by tsharma           #+#    #+#             */
-/*   Updated: 2022/04/27 20:10:48 by tsharma          ###   ########.fr       */
+/*   Updated: 2022/04/28 20:27:37 by tsharma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	**ft_split(char const *s, char c)
+static size_t	counter(const char *s, char c)
 {
 	size_t	count;
+	size_t	flag;
+	size_t	i;
+
+	i = 0;
+	flag = 0;
+	count = 0;
+	while (s[i] != '\0')
+	{
+		if (s[i] != c && flag == 0)
+		{
+			flag = 1;
+			count++;
+		}
+		else if (s[i] == c)
+			flag = 0;
+		i++;
+	}
+	return (count);
+}
+
+static void	length_count(size_t count, const char *s, char c, size_t *sc)
+{
 	size_t	i;
 	size_t	j;
 	size_t	k;
-	size_t	*size_count;
-	char	**res;
 
-	count = 1;
-	i = 0;
-	while (s[i])
-	{
-		if (s[i] == c)
-			count++;
-		i++;
-	}
-	size_count = (int *)malloc(sizeof(int) * count);
-	res = (char **)malloc((sizeof(char **)) * count);
-	if (!res)
-		return (NULL);
 	i = 0;
 	j = 0;
 	k = 0;
-	while (s[i])
+	while (s[i] != '\0' && k < count)
 	{
-		if (s[i] == c)
-		{
-			size_count[k] = i - j;
-			j = i;
-			k++;
-		}
-		i++;
+		while (s[i] && s[i] == c)
+			i++;
+		j = i;
+		while (s[i] && s[i] != c)
+			i++;
+		sc[k] = i - j;
+		k++;
 	}
+}
+
+static void	copy_word(char *dest, char *src, size_t size)
+{
+	size_t	i;
+
 	i = 0;
-	while (i < count)
+	while (i < size)
 	{
-		printf("%d: %d\n", i, size_count[i]);
+		dest[i] = src[i];
 		i++;
 	}
+	dest[i] = '\0';
+}
+
+static char	**split_it(const char *s, char c, size_t count, size_t *size_count)
+{
+	size_t	i;
+	size_t	k;
+	char	**res;
+
+	res = (char **)malloc((sizeof(char **)) * (count + 1));
+	if (!res)
+		return (NULL);
+	i = 0;
+	k = 0;
+	while (k < count)
+	{
+		while (s[i] == c)
+			i++;
+		res[k] = (char *)malloc(sizeof(char) * size_count[k] + 1);
+		copy_word(res[k], (char *)&s[i], size_count[k]);
+		i = i + size_count[k] + 1;
+		k++;
+	}
+	free(size_count);
+	res[count] = NULL;
 	return (res);
 }
 
-int	main(void)
+char	**ft_split(char const *s, char c)
 {
-	ft_split("HELLO WORLD", 'O');
+	size_t	count;
+	size_t	*size_count;
+
+	count = counter(s, c);
+	size_count = (size_t *)malloc(sizeof(size_t) * count);
+	length_count(count, s, c, size_count);
+	return (split_it(s, c, count, size_count));
 }
