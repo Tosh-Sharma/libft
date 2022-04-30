2ca0243356284512c6cf13d86eb1f7c9c5485640
Toshkumar Sharma <tsharma@c2r7p1.42nice.fr>
2022-04-30 16:50:46 +0200

    Updated strlcat to work.

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: tsharma <tsharma@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/04/24 16:49:46 by tsharma           #+#    #+#             */
-/*   Updated: 2022/04/25 16:28:15 by tsharma          ###   ########.fr       */
+/*   Updated: 2022/04/30 16:44:43 by tsharma          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,19 +15,21 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	dest_size;
+	size_t	src_size;
 	size_t	i;
 
 	dest_size = ft_strlen(dst);
+	src_size = ft_strlen(src);
 	i = 0;
 	if (dstsize == 0)
-		return (ft_strlen(src));
+		return (src_size);
+	if (dest_size >= dstsize)
+		return (dstsize + src_size);
 	while (src[i] != '\0' && i < (dstsize - dest_size - 1))
 	{
 		dst[dest_size + i] = src[i];
 		i++;
 	}
 	dst[dest_size + i] = '\0';
-	if (dest_size > dstsize)
-		return (dstsize + ft_strlen(src));
-	return (dest_size + ft_strlen(src));
+	return (dest_size + src_size);
 }
